6b20012df1d6c46c8863393bb12b7b636aa2f142
Davi Camarinha <triblood@users.noreply.github.com>
2018-12-26 19:19:14 -0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: 'HiQDaAEjANhW5kF0x4REUBTMp',  
-    consumer_secret: 'C7Yeirzjwh4gOfLpXd7BMwbAH5hTIh6LdMR8kel5u9ySo4o9Wy',
-    access_token: '40267429-rTDgfsoWNz8kwEPAd50yZ5Y0PyKerbeY5pdQo5vSW',  
-    access_token_secret: 'mxsOIXMhwEAd0tpHVVq9lUXbYPJwmgnnLWc8886fy9PDM'
+    consumer_key: '',  
+    consumer_secret: '',
+    access_token: '',  
+    access_token_secret: ''
 }
